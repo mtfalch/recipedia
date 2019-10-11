6ec4a7a05b4039d4bd4038fa84946aa00f1fa172
Alphaharrius <47113671+Alphaharrius@users.noreply.github.com>
2019-10-11 18:43:55 +0800

    Sign Up Files
    Hiiiii

---
+++ sign_up/validate_email.php
@@ -0,0 +1,23 @@
+<?php
+
+    error_reporting(E_ERROR | E_PARSE);
+    $serverName = "localhost";
+    $serverUserName = "root";
+    $serverPassword = "";
+
+    $con = mysqli_connect($serverName, $serverUserName, $serverPassword);
+    mysqli_select_db($con, "recipedia");
+
+    $email = $_POST["email"];
+
+    $sql = 'select count(*) as cnt from users where email = "' 
+        . $email . '"';
+
+    $res = mysqli_query($con, $sql) or die("Failed: " . mysqli_error());
+    $cnt = mysqli_fetch_array($res)['cnt'];
+
+    $obj -> singular = $cnt;
+    $json = json_encode($obj);
+    echo $json;
+
+?>
