03caa4fc9a941ca0ba48fb49428ea62ed9720164
catyingyingcoco <55784599+catyingyingcoco@users.noreply.github.com>
2019-10-13 01:37:53 +0800

    Add files via upload

---
+++ login_v2/setCookie.php
@@ -0,0 +1,16 @@
+<?php
+//set cookie
+if(!isset($_COOKIE["userlogin"]))
+{
+  $cookie_count = 1;
+  setcookie("userlogin",$_POST['user'], time()+60*60*24);
+  setcookie("pw",$_POST['password'], time()+60*60*24);
+  setcookie("count",$cookie_count, time()+60*60*24);
+  header("Location:http://localhost/miniproject/process.php");
+}
+else {
+  $cookie_count = ++$_COOKIE['count'];
+  setcookie("count",$cookie_count, time()+60*60*24);
+  header("Location:http://localhost/miniproject/process.php");
+}
+?>
