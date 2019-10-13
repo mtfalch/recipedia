ae1245c44b451d790cc6e7527609a00b039960a4
Alphaharrius <47113671+Alphaharrius@users.noreply.github.com>
2019-10-13 23:50:08 +0800

    Update sign_up.php

--- sign_up/sign_up.php
+++ sign_up/sign_up.php
@@ -7,14 +7,14 @@
     $con = mysqli_connect($serverName, $serverUserName, $serverPassword);
     mysqli_select_db($con, "recipedia");
 
-    $userID = $_POST["email"];
-    $password = $_POST["password"];
+    $userID = mysql_real_escape_string($_POST["email"]);
+    $password = mysql_real_escape_string($_POST["password"]);
 
-    $sql = 'insert into users (email, password) values ("' 
-        . $userID
-        . '","' 
-        . $password
-        . '")';
+    $sql = "INSERT into users (email, password) values (' 
+        $userID
+        ',' 
+        $password
+        ');
     mysqli_query($con, $sql) or die("Failed: " . mysqli_error());
 
     $obj -> status = "SUCCESS";
