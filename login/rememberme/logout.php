4e199d20630615958386a23ef2d6b8da77a06b0c
tiger <szeyk123@gmail.com>
2019-11-08 19:08:23 +0800

    123

---
+++ login/rememberme/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+session_destroy();
+if(isset($_COOKIE["userID"]))
+{
+ {
+ setcookie ("userID", "");
+ }
+ if(isset($_COOKIE["password"]))
+ {
+ setcookie ("password", "");
+ }
+}
+header("Location:login.php");
+
+?>
