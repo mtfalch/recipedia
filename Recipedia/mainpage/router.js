72aad95014d2988b4dc1299ea0b432dc40da0537
matthew <54795865+cccmatthew@users.noreply.github.com>
2019-11-21 13:14:02 +0800

    <Set cookies JS Version>

--- Recipedia/mainpage/router.js
+++ Recipedia/mainpage/router.js
@@ -1,24 +1,25 @@
+function routerStart(startPage){
+    window.prev = "";
+    window.routerInterval = 
+        this.setInterval(
+            function(){
+                var current = location.href.split('#')[1];
+                if(!current)
+                    current = startPage;
+                if(current == prev)
+                    return;
+                prev = current;
+                switch(current){
+                    case 'recipes': recipesButton.onclick(); break;
+                    case 'search': searchButton.onclick(); break;
+                    case 'upload': uploadButton.onclick(); break;
+                    case 'settings': settingsButton.onclick(); break;
+                }
+            },
+            100
+        );
+}
 
-var prev = "";
-
-window.onload = 
-    function(){
-        window.routerInterval = 
-            this.setInterval(
-                function(){
-                    var current = location.href.split('#')[1];
-                    if(!current)
-                        current = 'recipes';
-                    if(current == prev)
-                        return;
-                    prev = current;
-                    switch(current){
-                        case 'recipes': recipesButton.onclick(); break;
-                        case 'search': searchButton.onclick(); break;
-                        case 'upload': uploadButton.onclick(); break;
-                        case 'settings': settingsButton.onclick(); break;
-                    }
-                },
-                100
-            );
-    }
+function routerStop(){
+    clearInterval(window.routerInterval);
+}
